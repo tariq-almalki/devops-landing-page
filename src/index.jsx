88a3123b2edc82bc8d8e399fa26b48708aba8cac
tariq-almalki <e.tariq.almalki@gmail>
2022-10-31 06:43:43 +0300

    Excluding 'js' folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
-// import './js/scrollbar.js';
 import './base-styles.css';
 
 const root = createRoot(document.getElementById('root'));
