afffb924d41f2adc2d30a92a8928553e5f805a5b
tariq-almalki <e.tariq.almalki@gmail>
2022-11-01 07:29:25 +0300

    Change divs inside main to sections, Delete Main component because it is not used

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,9 @@
-// import { NavBar } from '../NavBar/NavBar.jsx';
 import classes from './HomePage.module.css';
 
 export function HomePage() {
     return (
-        <div className={classes.homepage}>
-            <div className={classes.div1}>
+        <main className={classes.homepage}>
+            <section className={classes.section1}>
                 <h1 className={classes.h1}>
                     Welcome to <span className={classes.span}> DevOps Platform</span>
                 </h1>
@@ -12,7 +11,15 @@ export function HomePage() {
                     A Rich DevOps Platform with many services. Enabling teams to deliver faster, more frequently and in
                     more reliable fashion.
                 </p>
-            </div>
-        </div>
+            </section>
+            <section className={classes.section2}>
+                <video
+                    src='https://wiki.elm.sa/display/TAKS/Dojo+Digital+Library?preview=/99575719/99575718/Week-1_Session-1_2020-11-08.mp4'
+                    poster='../../images/misc/video-image.jpg'
+                    height={504}
+                    width={860}
+                ></video>
+            </section>
+        </main>
     );
 }
--- src/components/HomePage/Main/Main.jsx
+++
@@ -1,5 +0,0 @@
-import classes from './Main.module.css';
-
-export function Main(props) {
-    return <main className={classes.main}>{props.children}</main>;
-}
