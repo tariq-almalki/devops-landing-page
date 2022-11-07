42bf116d17536e787076caeaeb4b04ef20d19cc0
tariq-almalki <e.tariq.almalki@gmail>
2022-11-07 11:11:07 +0300

    changing img url

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -15,39 +15,44 @@ export function Services(props) {
                 <p className={classes.p}>
                     Plan smarter, collaborate better, and ship faster with a set of modern DevOps services
                 </p>
-                <div className={classes.div}>
-                    <ul className={classes.ul}>
-                        <li className={classes.li1}>
-                            <img
-                                className={classes['li-img']}
-                                src={props.services[0].src}
-                                style={props.services[0].style}
-                                alt=''
-                            />
-                            <h1 className={classes['service-h1']}>{props.services[0].h1}</h1>
-                            <p className={classes['service-p']}>{props.services[0].p}</p>
-                        </li>
-                        <li className={classes.li2}>
-                            <img
-                                className={classes['li-img']}
-                                src={props.services[1].src}
-                                style={props.services[1].style}
-                                alt=''
-                            />
-                            <h1 className={classes['service-h1']}>{props.services[1].h1}</h1>
-                            <p className={classes['service-p']}>{props.services[1].p}</p>
-                        </li>
-                        <li className={classes.li3}>
-                            <img
-                                className={classes['li-img']}
-                                src={props.services[2].src}
-                                style={props.services[2].style}
-                                alt=''
-                            />
-                            <h1 className={classes['service-h1']}>{props.services[2].h1}</h1>
-                            <p className={classes['service-p']}>{props.services[2].p}</p>
-                        </li>
-                    </ul>
+                <div className={classes.div1}>
+                    <a href='https://jira.elm.sa' className={classes.a1} target='_blank' rel='noreferrer'>
+                        <div style={props.services[0].imgDivStyles}>
+                            <img src={props.services[0].src} style={props.services[0].imgStyles} alt='' />
+                        </div>
+                        <div style={props.services[0].h1DivStyles}>
+                            <h1 style={props.services[0].h1Styles}>{props.services[0].h1}</h1>
+                        </div>
+                        <div style={props.services[0].pDivStyles}>
+                            <p style={props.services[0].pStyles}>{props.services[0].p}</p>
+                        </div>
+                    </a>
+                </div>
+                <div className={classes.div2}>
+                    <a href='https://jira.elm.sa' className={classes.a2} target='_blank' rel='noreferrer'>
+                        <div style={props.services[1].imgDivStyles}>
+                            <img src={props.services[1].src} style={props.services[1].imgStyles} alt='' />
+                        </div>
+                        <div style={props.services[1].h1DivStyles}>
+                            <h1 style={props.services[1].h1Styles}>{props.services[1].h1}</h1>
+                        </div>
+                        <div style={props.services[1].pDivStyles}>
+                            <p style={props.services[1].pStyles}>{props.services[1].p}</p>
+                        </div>
+                    </a>
+                </div>
+                <div className={classes.div3}>
+                    <a href='https://jira.elm.sa' className={classes.a3} target='_blank' rel='noreferrer'>
+                        <div style={props.services[2].imgDivStyles}>
+                            <img src={props.services[2].src} style={props.services[2].imgStyles} alt='' />
+                        </div>
+                        <div style={props.services[2].h1DivStyles}>
+                            <h1 style={props.services[2].h1Styles}>{props.services[2].h1}</h1>
+                        </div>
+                        <div style={props.services[2].pDivStyles}>
+                            <p style={props.services[2].pStyles}>{props.services[2].p}</p>
+                        </div>
+                    </a>
                 </div>
             </main>
         </div>
